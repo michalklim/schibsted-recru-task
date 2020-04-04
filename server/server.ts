8ea11e007a500667ed5b0e071a43523eb844211f
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-04-05 00:34:35 +0200

    chore(server): add morgan logging

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,19 @@ import express from 'express'
 import es6promise from 'es6-promise'
 import 'isomorphic-fetch'
 import cors from 'cors'
+import morgan from "morgan";
 import {itemsRouter} from "./items";
+
 
 es6promise.polyfill()
 
 const app = express()
+
 app.disable('x-powered-by')
+
+const morganFormat = process.env.CONTEXT === 'production' ? 'tiny' : 'dev'
+app.use(morgan(morganFormat))
+
 app.use(cors({
   origin: process.env.URL || 'http://localhost:3000'
 }))
