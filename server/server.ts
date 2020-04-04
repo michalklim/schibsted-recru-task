46c6bd3815d4f2852009de2939f5b96101b7a5c3
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-04-05 00:30:36 +0200

    chore(server): setup cors

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import es6promise from 'es6-promise'
 import 'isomorphic-fetch'
-
+import cors from 'cors'
 import {itemsRouter} from "./items";
 
 es6promise.polyfill()
 
 const app = express()
 app.disable('x-powered-by')
+app.use(cors({
+  origin: process.env.URL || 'http://localhost:3000'
+}))
 
 app.use('/api/items', itemsRouter)
 export {app}
