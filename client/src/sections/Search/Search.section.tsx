b97728ca1993c60b7b0127e7138632d647bff192
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-04-05 22:32:38 +0200

    style(client): remove eslint warnings

--- client/src/sections/Search/Search.section.tsx
+++ client/src/sections/Search/Search.section.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useRef } from 'react'
+import React, { FunctionComponent, useEffect } from 'react'
 import { RouteComponentProps, useParams } from '@reach/router'
 import styled from 'styled-components'
 import { useSpring } from 'react-spring'
@@ -13,17 +13,19 @@ const Section = styled.div`
 `
 
 export const SearchSection: FunctionComponent<RouteComponentProps> = () => {
-  const { _, y } = useWindowScroll()
+  const { y } = useWindowScroll()
   const [, setY] = useSpring(() => ({ y }))
   const params = useParams()
 
+  /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
     setY({
       y: window.innerHeight,
       from: { y },
       onFrame: (props) => window.scroll(0, props.y),
     })
-  }, [params?.term])
+  }, [params.term])
+  /* eslint-enable react-hooks/exhaustive-deps */
 
   return <Section>search section</Section>
 }
